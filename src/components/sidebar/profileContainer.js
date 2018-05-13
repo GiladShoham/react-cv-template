53d45bd395c7a15b3cf814819610818cc4df80dc
Gilad Shoham <shoham.gilad@gmail.com>
2018-05-13 13:16:31 +0300

    add profile container

--- src/components/sidebar/profileContainer.js
+++ src/components/sidebar/profileContainer.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import '../../assets/theme/styles.css';
 
+/**
+ * @render react
+ * @name ProfileContainer
+ * @description ProfileContainer description
+ * @example
+ * <ProfileContainer name="Gilad Shoham" title="Dev & Open source Leader" imagePath="https://en.gravatar.com/userimage/51779316/975a7ab5400cfac3e90be71f242241eb.jpg?size=200" />
+ */
 export default class ProfileContainer extends Component {
   renderProfilePicture(imagePath) {
     if (imagePath) {
