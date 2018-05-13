caae1ca4ff240a8894f02dc08848d555a5c24694
Gilad Shoham <shoham.gilad@gmail.com>
2018-05-13 14:20:44 +0300

    edit data

--- src/data/careerProfile.js
+++ src/data/careerProfile.js
@@ -1,7 +1,14 @@
 export const careerProfile = {
   sectionTitle: 'Career Profile',
   icon: null,
-  description: '<p>I\'m coding something since I was 14. In the beginning I was creating game servers for popular games, after that I started with php and created some basic apps for small-sized companies. Then I decided to train myself. And now, I have successfully finished a good many projects for companies, universities and my government you can see the project details <a href="#projects">below.</a> So, may be we can work together!</p>'
+  description: `<p>I believe that software development is a tool to improve human life.
+  <p>I find software to be the most powerful way to share value In order to connect people and improve the quality of all our lives.</p>
+  <p>Software engineering for me is like painting or sculpting, it’s a way to take a complicated and beautiful structure lay, connecting all the dots piece by piece, and create a masterpiece. It’s more than just art, it’s life.
+  That’s why I wake up with a smile, and keep it on all day long. I am so lucky to be paid for doing what I love the most.
+  <br>That’s what makes me a great engineer. <br>I’m not smarter than others, I just really care.</p>
+  <p>I love what I do and do what I love, so I don’t play the nine to five game, I live it 24/7.
+  This rush makes me develop community projects like Stunity and learn new paradigms and tools with the passion of a child getting a new toy running around and having fun without the need for any additional incentive.</p>
+  I share my beliefs with others to inspire them, and some follow. I also learn from others. That’s what makes me a true leader.</p>`
 };
 
 export default careerProfile;
